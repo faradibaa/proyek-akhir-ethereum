686b93f22931da57e89d422e54e7358c40f4bf6d
DrowsyLad <tsumwashere@student.ub.ac.id>
2022-12-22 00:56:43 +0700

    Progress front-end

--- src/main.js
+++ src/main.js
@@ -8,12 +8,24 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faWallet } from '@fortawesome/free-solid-svg-icons'
+
+/* Import bootstrap-vue */
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 /* add icons to the library */
-library.add(faUserSecret)
+library.add(faWallet)
 
+// Make BootstrapVue available throughout your project
+// createApp(App).use(BootstrapVue)
+// // Optionally install the BootstrapVue icon components plugin
+// createApp(App).use(IconsPlugin)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
+// .component('bootstrap-vue', BootstrapVue)
+// .component('bootstrap-vue-icons-plugin', IconsPlugin)
 .mount('#app')
